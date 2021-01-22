8d8de2e606fa445c7266375e1419433b5fcc9d8c
Kelly Costa <kellydosocorro@gmail.com>
2021-01-22 15:00:54 -0300

    Adiciona comentários no HTML de Enderecos.vue

--- src/views/Enderecos.vue
+++ src/views/Enderecos.vue
@@ -1,23 +1,29 @@
 <template>
   <b-container fluid="md" class="about">
-    <!-- <h1>This is an about page</h1> -->
+    <!-- Começo da tabela de endereços -->
     <b-table striped hover :items="enderecos" :fields="fields" show-empty>
+      <!-- Mensagem para exibição quando a tabela está vazia -->
       <template #empty>
         <h6>Não há endereços cadastrados para exibição</h6>
       </template>
+      <!-- Personalização do campo Opções -->
       <template v-slot:cell(opcoes)="{ item }">
         <b-button-toolbar :aria-label="'Opções para o cep ' + item.cep">
           <b-button-group size="sm" class="options-toolbar">
+            <!-- Botão para editar o endereço -->
             <b-button variant="link" @click="editarEndereco(item)">
               <b-icon icon="pencil-fill" />
             </b-button>
+            <!-- Botão para excluir o endereço -->
             <b-button variant="link" @click="deletarEndereco(item)">
               <b-icon icon="trash" />
             </b-button>
           </b-button-group>
         </b-button-toolbar>
       </template>
+      <!-- Fim da personalização do campo Opções -->
     </b-table>
+    <!-- Fim da tabela de enderecos -->
   </b-container>
 </template>
 <script>
