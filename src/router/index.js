547472ecc0805cdebac4ad0280cc67de371f9d6b
Kelly Costa <kellydosocorro@gmail.com>
2021-01-22 23:26:27 -0300

    Adiciona página para erro

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     name: "Enderecos",
     component: () =>
       import(/* webpackChunkName: "Enderecos" */ "../views/Enderecos.vue")
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: () => import("../views/NotFound.vue")
   }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container fluid="md" class="about">
+    <figure class="notfound-banner">
+      <img src="../assets/images/3959810.jpg" width="500" />
+      <figcaption>
+        <a href="https://www.freepik.com/vectors/travel" target="_blank">
+          Illustration created by stories - www.freepik.com
+        </a>
+      </figcaption>
+    </figure>
+  </b-container>
+</template>
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
+<style scoped>
+.notfound-banner figcaption {
+  text-align: center;
+}
+.notfound-banner figcaption a {
+  font-size: 0.65rem;
+  color: #b5b5b5;
+}
+</style>
